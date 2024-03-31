a0606e5cf71a76ea9c21f82a2f78eb7b5677f464
YuraSS23 <nikitsenko_yura@mail.ru>
2024-03-31 11:12:31 +0300

    75

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,15 +1,70 @@
 import React from 'react';
 import s from './Login.module.css';
+import {Field, Formik} from 'formik';
+
+type AuthFormType = {
+    email: string
+    password: string
+    rememberMe: boolean
+}
+
+const usersLoginFormValidate = (values: any) => {
+    const errors = {};
+    return errors;
+}
 
 export const LoginForm = () => {
+    const submit =(values: AuthFormType, {setSubmitting}: {setSubmitting: (isSubmitting: boolean) => void}) => {
+        const filter: AuthFormType = {
+            email: values.email,
+            password: values.password,
+            rememberMe: values.rememberMe
+        }
+        console.log(filter)
+        setSubmitting(false)
+    }
     return (
-        <form className={s.loginForm}>
-            <input placeholder={'Login'}/>
-            <input placeholder={'Password'}/>
-            <div>
-                <input type={'checkbox'}/><span>Remember me</span>
-            </div>
-            <button type={'submit'}>Login</button>
-        </form>
+        <Formik
+            initialValues={{email: '', password: '', rememberMe: false}}
+            validate={usersLoginFormValidate}
+            onSubmit={submit}
+        >
+            {({
+                  values,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+              }) => (
+                <form onSubmit={handleSubmit} className={s.loginForm}>
+                    <Field
+                        type="text"
+                        name="email"
+                        placeholder={"email"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                    />
+                    <Field
+                        type="password"
+                        name="password"
+                        placeholder={"password"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                    />
+                    <label>
+                        <Field
+                            type="checkbox"
+                            name="rememberMe"
+                            checked={values.rememberMe}
+                        /> Remember me
+                    </label>
+                    <button type="submit" disabled={isSubmitting}>
+                        Login
+                    </button>
+                </form>
+            )}
+        </Formik>
     );
 };
