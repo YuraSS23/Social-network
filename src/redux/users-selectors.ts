9f7ff9daac492ddfbcaec54601f4f23bfb46449e
YuraSS23 <nikitsenko_yura@mail.ru>
2024-04-09 10:35:17 +0300

    82-83

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,5 +1,10 @@
 import {RootStateType} from './redux-store';
+import {createSelector} from "reselect";
 
-export const getUsersPage = (state: RootStateType)=> {
+export const getUsersPageSelector = (state: RootStateType)=> {
     return state.usersPage
 }
+
+export const getUsersPage = createSelector(getUsersPageSelector, (usersPage)=> {
+    return {...usersPage}
+})
